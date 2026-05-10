98d6729bba0dcef63d6795a17a64ecb7bc67fa2b
Pallab Karmakar <ookarmakarpallab1212@gmail.com>
2026-05-11 02:09:27 +0530

    refactor: remove deprecated code from user data validation schema

--- backend/src/app/auth/validator.ts
+++ backend/src/app/auth/validator.ts
@@ -1,8 +1,15 @@
-// use zod for user data validation
 import { z } from "zod";
 
 export const signupSchema = z.object({
-    fullname: z.string().min(3, "Full name must be at least 3 characters long"),
-    email: z.string().email("Invalid email address"),
-    password: z.string().min(6, "Password must be at least 6 characters long"),
+    fullname: z.string().min(3, {
+        message: "Full name must be at least 3 characters long",
+    }),
+
+    email: z.email({
+        message: "Invalid email address",
+    }),
+
+    password: z.string().min(6, {
+        message: "Password must be at least 6 characters long",
+    }),
 });
