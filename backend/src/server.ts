afc7c797315a35beb63783a98812d65e8ae5f5d4
Pallab Karmakar <ookarmakarpallab1212@gmail.com>
2026-05-11 02:21:52 +0530

    feat: add dotenv configuration and MongoDB connection setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,14 @@
 import { createServer } from 'node:http';
 import createApp from './app/app.js';
+import dotenv from "dotenv";
+dotenv.config({
+    path: './.env',
+    quiet: true,
+});
+import { connectDB } from './app/db/conn.js';
 
-const startServer = () => {
+const startServer = async () => {
+    await connectDB();
     const server = createServer(createApp());
     const PORT = process.env.PORT || 3000;
     server.listen(PORT, () => {
