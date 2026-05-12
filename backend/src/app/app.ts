458839b32aeed5faa5c890af972a9749338037ba
Pallab Karmakar <ookarmakarpallab1212@gmail.com>
2026-05-12 18:11:47 +0530

    feat: add CORS support to enable cross-origin requests

--- backend/src/app/app.ts
+++ backend/src/app/app.ts
@@ -3,10 +3,15 @@ import authRoutes from './module/auth/routes.js';
 import pollRoutes from './module/poll/routes.js';
 import publicRoutes from './module/public/routes.js';
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 const createApp = () => {
     const app = express();
     app.use(express.json());
     app.use(cookieParser());
+    app.use(cors({
+        origin: 'http://localhost:5173',
+        credentials: true
+    }))
 
     // create a health route
     app.get('/health', (req, res: Response) => {
